c601ce2fa8943e59b6622d910f8709e03d431d50
Bartek Zychowski <bartis1313@gmail.com>
2024-07-29 11:50:58 +0200

    it got deleted

--- BattleCon/src/BattleCon.js
+++ BattleCon/src/BattleCon.js
@@ -1,3 +1,30 @@
+/*
+ Copyright 2013 Daniel Wirtz <dcode@dcode.io>
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+ var events = require("events"),
+ net = require("net"),
+ Message = require("./BattleCon/Message.js");
+
+/**
+* Constructs a new BattleCon instance.
+* @exports BattleCon
+* @param {string} host Hostname or IP
+* @param {number} port Port
+* @param {string} pass RCON password
+* @constructor
+* @extends events.EventEmitter
+*/
+
 class BattleCon extends events.EventEmitter {
     constructor(host, port, pass) {
         super();
