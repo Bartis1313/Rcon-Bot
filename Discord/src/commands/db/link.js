6e6ebd3fba641378d5efa9cd53c8f760e46db7fe
bartis1313 <bartis1313@gmail.com>
2025-02-21 13:34:40 +0100

    fix link for complex mode

--- Discord/src/commands/db/link.js
+++ Discord/src/commands/db/link.js
@@ -1,6 +1,6 @@
-const { EmbedBuilder, SlashCommandBuilder, MessageFlags } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const geoip = require('geoip-lite');
-import { Helpers, DiscordLimits } from '../../helpers/helpers';
+import { Helpers, DiscordLimits } from '../../helpers/helpers'
 import DBHelper from '../../helpers/dbHelper';
 
 module.exports = class LinkCommand {
@@ -35,47 +35,85 @@ module.exports = class LinkCommand {
                     .setDescription("Give name of player")
                     .setRequired(true)
             )
+            .addBooleanOption(option =>
+                option.setName("complex")
+                    .setDescription("Find really ALL ips")
+                    .setRequired(false)
+            );
     }
 
-    async findLinkedAccounts(connection, playerName) {
+    async findLinkedAccounts(connection, playerName, easy) {
         try {
             return DBHelper.query(connection, `
                 WITH ActID AS (
-                    SELECT PlayerID FROM tbl_playerdata WHERE SoldierName = ?
+                    SELECT PlayerID, IP_Address FROM tbl_playerdata WHERE SoldierName = ?
                 ),
                 HistID AS (
                     SELECT PlayerID FROM SoldierName_history WHERE Old_SoldierName = ?
                 )
-                SELECT PlayerID FROM ActID
+                SELECT PlayerID, IP_Address FROM ActID
                 UNION
-                SELECT PlayerID FROM HistID;
+                SELECT PlayerID, NULL FROM HistID;
             `, [playerName, playerName])
-                .then(playerDataIDs => {
-                    if (!playerDataIDs.length) return [];
-                    const linkedPlayerIDs = playerDataIDs.map(row => row.PlayerID);
-                    return DBHelper.query(connection, `
-                    SELECT DISTINCT 
-                        pd.SoldierName AS original_soldierName,
-                        pd_linked.SoldierName AS linked_soldierName,
-                        ih.IP_Address,
-                        pd.GameID
-                    FROM ip_history ih
-                    JOIN tbl_playerdata pd ON ih.PlayerID = pd.PlayerID
-                    JOIN ip_history ih_linked ON ih.IP_Address = ih_linked.IP_Address
-                    JOIN tbl_playerdata pd_linked ON ih_linked.PlayerID = pd_linked.PlayerID
-                    WHERE ih.PlayerID IN (?);
-                `, [linkedPlayerIDs]);
-                })
-                .then(infos => infos
-                    .filter(row => row.original_soldierName !== row.linked_soldierName)
-                    .map(row => {
-                        const country = geoip.lookup(row.IP_Address)?.country || 'Err';
-                        return `${row.linked_soldierName} (IP: ${row.IP_Address}, Game ID: ${this.mapNames.get(row.GameID)}) Country: ${country}`;
-                    })
-                );
+                .then(async playerData => {
+                    if (!playerData.length) return { linkedAccounts: [], ipHistory: [] };
+
+                    const playerIDs = playerData.map(row => row.PlayerID);
+                    const currentIP = playerData.find(row => row.IP_Address)?.IP_Address || 'Unknown';
+
+                    const ipHistory = await DBHelper.query(connection, `
+                        SELECT DISTINCT IP_Address FROM ip_history WHERE PlayerID IN (?);
+                    `, [playerIDs]);
+
+                    let linkedAccounts = [];
+
+                    if (easy) {
+                        // easy - get directly only current
+                        linkedAccounts = await DBHelper.query(connection, `
+                            SELECT DISTINCT
+                                SoldierName AS linked_soldierName,
+                                IP_Address,
+                                GameID
+                            FROM tbl_playerdata
+                            WHERE IP_Address IN (
+                                SELECT IP_Address FROM tbl_playerdata WHERE PlayerID IN (?)
+                            );
+                        `, [playerIDs]);
+                    } else {
+                        // complex - based on ip history
+                        const ipList = ipHistory.map(row => row.IP_Address);
+                        linkedAccounts = await DBHelper.query(connection, `
+                            SELECT DISTINCT 
+                                pd.SoldierName AS original_soldierName,
+                                pd_linked.SoldierName AS linked_soldierName,
+                                ih.IP_Address,
+                                pd.GameID
+                            FROM ip_history ih
+                            JOIN tbl_playerdata pd ON ih.PlayerID = pd.PlayerID
+                            JOIN ip_history ih_linked ON ih.IP_Address = ih_linked.IP_Address
+                            JOIN tbl_playerdata pd_linked ON ih_linked.PlayerID = pd_linked.PlayerID
+                            WHERE ih.PlayerID IN (?);
+                        `, [playerIDs]);
+                    }
+
+                    const uniqueNames = new Set();
+                    const filteredLinkedAccounts = linkedAccounts.filter(el => {
+                        if (!uniqueNames.has(el.linked_soldierName)) {
+                            uniqueNames.add(el.linked_soldierName);
+                            return true;
+                        }
+                        return false;
+                    });
+
+                    return {
+                        linkedAccounts: Array.from(filteredLinkedAccounts),
+                        ipHistory: ipHistory.map(row => row.IP_Address),
+                        currentIP
+                    };
+                });
         } catch (error) {
             console.error('Error finding linked accounts:', error);
-            return [];
+            return { linkedAccounts: [], ipHistory: [] };
         }
     }
 
@@ -88,9 +126,10 @@ module.exports = class LinkCommand {
         const server = interaction.options.getString("server");
         const serverDB = this.dbsConfig.find(db => db.database === server);
         const playerName = interaction.options.getString("name");
+        const complex = interaction.options.getBoolean("complex");
 
         const linkedAccounts = await DBHelper.processServer(serverDB, async (connection) => {
-            return await this.findLinkedAccounts(connection, playerName);
+            return await this.findLinkedAccounts(connection, playerName, !complex);
         });
 
         if (linkedAccounts.length === 0) {
@@ -98,12 +137,23 @@ module.exports = class LinkCommand {
             return;
         }
 
-        await interaction.editReply({ embeds: await this.buildEmbeds(interaction, serverDB, playerName, linkedAccounts) });
+        const embeds = await this.buildEmbeds(interaction, serverDB, playerName, linkedAccounts);
+        await interaction.editReply({ embeds: embeds.slice(0, 10) });
+
+        if (embeds.length > 10) {
+            const remainingEmbeds = embeds.slice(10);
+            for (let i = 0; i < remainingEmbeds.length; i += 10) {
+                const chunk = remainingEmbeds.slice(i, i + 10);
+                await interaction.followUp({ embeds: chunk });
+            }
+        }
     }
 
-    async buildEmbeds(messageOrInteraction, serverDB, playerName, linkedAccounts) {
+    async buildEmbeds(messageOrInteraction, serverDB, playerName, linkedData) {
         const user = Helpers.isCommand(messageOrInteraction) ? messageOrInteraction.user : messageOrInteraction.author;
-        
+        const { linkedAccounts, ipHistory, currentIP } = linkedData;
+
+        const embeds = [];
         const chunkArray = (arr, size) => {
             const chunks = [];
             for (let i = 0; i < arr.length; i += size) {
@@ -112,35 +162,41 @@ module.exports = class LinkCommand {
             return chunks;
         };
 
-        const accountChunks = chunkArray(linkedAccounts, DiscordLimits.accountsPerField);
-
-        const embeds = [];
-        let currentEmbed = new EmbedBuilder()
-            .setColor('Green')
-            .setTitle(`Linked Accounts for ${playerName} - ${serverDB.database} DB`)
+        const ipEmbed = new EmbedBuilder()
+            .setColor('Blue')
+            .setTitle(`${playerName} - ${serverDB.database} DB (${currentIP})`)
             .setFooter({ text: `Requested by ${user.username}` })
             .setTimestamp();
 
+        let descIp = "🔹 Previous ips\n\n";
+
+        const splitedIps = chunkArray(ipHistory, 4);
+
+        splitedIps.forEach(ip => {
+            descIp += `[ ${ip.join(', ')} ]\n`;
+        });
+
+        ipEmbed.setDescription(Helpers.truncateString(descIp, DiscordLimits.maxDescriptionLength));
+
+        embeds.push(ipEmbed);
+
+        const accountChunks = chunkArray(linkedAccounts, DiscordLimits.maxChoices);
+
         for (let i = 0; i < accountChunks.length; i++) {
             const chunk = accountChunks[i];
-            const fieldValue = Helpers.truncateString(chunk.map(account => `• ${account}`).join('\n'), DiscordLimits.maxFieldLength);
+            const embed = new EmbedBuilder()
+                .setColor('Green')
+                .setTitle(i === 0 ? `Linked Accounts for ${playerName}` : null)
+                .setFooter({ text: `Requested by ${user.username}` })
+                .setTimestamp();
 
-            if (currentEmbed.data.fields && currentEmbed.data.fields.length >= DiscordLimits.maxFieldsPerEmbed) {
-                embeds.push(currentEmbed);
-                currentEmbed = new EmbedBuilder()
-                    .setColor('Green')
-                    .setFooter({ text: `Requested by ${user.username}` })
-                    .setTimestamp();
-            }
-
-            currentEmbed.addFields({
-                name: `Accounts (Part ${i + 1})`,
-                value: fieldValue,
+            let str = '';
+            chunk.forEach((account, index) => {
+                str += `• ${account.linked_soldierName} (${account.IP_Address} ${geoip.lookup(account.IP_Address).country}, ${this.mapNames.get(account.GameID)})\n`;
             });
-        }
+            embed.setDescription(str);
 
-        if (currentEmbed.data.fields && currentEmbed.data.fields.length > 0) {
-            embeds.push(currentEmbed);
+            embeds.push(embed);
         }
 
         return embeds;
